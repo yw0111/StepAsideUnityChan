4728fddf6fb0cb427597b1497318ff78c8b1d847
Lawrence Jeong <yw0111@gmail.com>
2019-03-11 21:30:50 +0900

    StepAsideUnityChan
    DestroyScriptを追加して、各Prefabに付けました。

---
+++ Assets/DestroyScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyScript : MonoBehaviour {
+private GameObject MyCamera;
+
+	// Use this for initialization
+	void Start () {
+        this.MyCamera = GameObject.Find("Main Camera");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.MyCamera.transform.position.z >= this.transform.position.z)
+        Destroy(gameObject);
+    }
+}
